01f729baf22a5ff379b1248dd06fd2906043bf4f
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2023-09-05 20:25:41 -0300

    conclusão do priemiro modulo do l

--- controle-series/resources/views/seasons/index.blade.php
+++ controle-series/resources/views/seasons/index.blade.php
@@ -1,5 +1,7 @@
 <x-layout title="Temporadas de {!! $series->nome !!}">
+   
     <ul class="list-group">
+    
         @foreach ($seasons as $key => $season)
             <li class="list-group-item d-flex justify-content-between align-items-center">
                   Temporada {{ $season->number }}
